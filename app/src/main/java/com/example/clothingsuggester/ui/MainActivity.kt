dd3e6844aad7dc1dda3d65c315efc739653a551b
Zeyad Abdullah <zeyadAbdullah@gmail.com>
2023-04-12 02:15:00 +0200

    update main activity with shared pref

--- app/src/main/java/com/example/clothingsuggester/ui/MainActivity.kt
+++ app/src/main/java/com/example/clothingsuggester/ui/MainActivity.kt
@@ -1,29 +1,45 @@
 package com.example.clothingsuggester.ui
 
+import android.os.Build
+import androidx.annotation.RequiresApi
 import com.example.clothingsuggester.data.DataManager
 import com.example.clothingsuggester.databinding.ActivityMainBinding
+import com.example.clothingsuggester.domain.Cloth
 import com.example.clothingsuggester.domain.WeatherCodes
 import com.example.clothingsuggester.domain.WeatherData
 import com.example.clothingsuggester.domain.WeatherValues
 import com.example.clothingsuggester.ui.adapters.ClothesAdapter
 import com.example.clothingsuggester.util.ApiRequest
+import com.example.clothingsuggester.util.SharedPref
 import com.example.clothingsuggestor.ui.BaseActivity
 import com.google.gson.Gson
 import okhttp3.Response
+import java.time.LocalDateTime
 import kotlin.math.roundToInt
 
 class MainActivity : BaseActivity<ActivityMainBinding>(ActivityMainBinding::inflate) {
 
     override fun setup() {
+        SharedPref.initSharedPref(this)
         ApiRequest.currentWeatherStateRequest(::onFailure, ::onResponse)
-        binding.recyclerRecommendedOutfit.adapter = ClothesAdapter(DataManager.clothes)
+    }
+
+    private fun initRecyclerView(clothes:List<Cloth>) {
+        binding.recyclerRecommendedOutfit.adapter = ClothesAdapter(clothes)
     }
 
     private fun onResponse(response: Response) {
         val weatherState = Gson().fromJson(response.body?.string(), WeatherData::class.java)
         runOnUiThread {
             updateUiData(weatherState.data.weatherValues)
+            updateRecommendedOutfit(weatherState.data.weatherValues.temperature)
         }
+    }
+
+    private fun updateRecommendedOutfit(temperature: Double) {
+        val recommendedOutfit =
+            DataManager.getRecommendedOutfit(temperature)
+        initRecyclerView(recommendedOutfit)
     }
 
     private fun updateUiData(weather: WeatherValues) {
@@ -36,7 +52,5 @@ class MainActivity : BaseActivity<ActivityMainBinding>(ActivityMainBinding::infl
         }
     }
 
-    private fun onFailure() {
-
-    }
+    private fun onFailure() {}
 }
--- app/src/main/java/com/example/clothingsuggester/util/Constants.kt
+++ app/src/main/java/com/example/clothingsuggester/util/Constants.kt
@@ -1,6 +1,7 @@
 package com.example.clothingsuggester.util
 
 object Constants {
-
+    const val SHARED_PREFERENCE_NAME = "SavedCloth"
+    const val KEY_CLOTH = "clothId"
     const val API_KEY = "4JYbxnTdi7GfNR682gbblZgWJ6vcjO2q"
 }
